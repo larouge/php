a4f55b2e7b6caddb93fbb7fa1f0dbef837491314
larouge <remi.rouget0@gmail.com>
2017-05-08 19:21:08 +0200

    Version avec le formulaire s'apelle lui-même dans un if

--- formulaire.php
+++ formulaire.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-</head>
-<form method="post" action="secret.php">
-    <p>
-        <label for="pass"> Mot de Passe </label> <input type="password" name="pass" id="pass" required/>
-        <input type="submit" value="Se connecter" />
-    </p>
-</form>
+<?php
+if (!isset($_POST['pass']) or $_POST['pass'] != 'kangourou')
+{
+?>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <form method="post" action="formulaire.php">
+        <p>
+            <label for="pass"> Mot de Passe </label> <input type="password" name="pass" id="pass" required/>
+            <input type="submit" value="Se connecter" />
+        </p>
+    </form>
+    </html>
+<?php
+}
+else
+{
+    echo 'voici le code de la NASA : fire';
+}
+?>
